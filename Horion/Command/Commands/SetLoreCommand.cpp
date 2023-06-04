04dc41c913cee426566552face76e555ab4e9622
marioCST <wiiumr@web.de>
2023-06-04 20:55:19 +0200

    Use setLore from ItemStack class

--- Horion/Command/Commands/SetLoreCommand.cpp
+++ Horion/Command/Commands/SetLoreCommand.cpp
@@ -6,12 +6,6 @@ SetLoreCommand::SetLoreCommand() : IMCCommand("setlore", "Sets the lore of an it
 SetLoreCommand::~SetLoreCommand() {
 }
 
-void setLore(ItemStack* stack, std::vector<std::string> lore) {
-	using constructor_t = void(__fastcall*)(ItemStack*, std::vector<std::string>);
-	static constructor_t func = reinterpret_cast<constructor_t>(FindSignature("48 8B C4 48 89 58 ? 48 89 70 ? 55 57 41 54 41 56 41 57 48 8D 68 ? 48 81 EC ? ? ? ? ? ? 70 C8 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 4C"));
-	func(stack, lore);
-}
-
 bool SetLoreCommand::execute(std::vector<std::string>* args) {
 	assertTrue(args->size() >= 2);
 
@@ -36,10 +30,7 @@ bool SetLoreCommand::execute(std::vector<std::string>* args) {
 		lore += args->at(i) + " ";
 	}
 
-	std::vector<std::string> loreList;
-	loreList.push_back(lore);
-
-	setLore(item, loreList);
+	item->setLore(lore);
 
 	clientMessageF("Set lore to '%s'!", lore);
 	return true;
