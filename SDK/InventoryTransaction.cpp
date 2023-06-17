1d250e35a53272f997f4f34348c4ca5dda49c26a
marioCST <wiiumr@web.de>
2023-06-17 14:38:33 +0200

    Fix fixInvStuff sig

--- SDK/InventoryTransaction.cpp
+++ SDK/InventoryTransaction.cpp
@@ -7,7 +7,7 @@ void InventoryTransactionManager::addInventoryAction(const InventoryAction& acti
 }
 
 void InventoryAction::fixInventoryStuff(ItemDescriptor* a1, ItemStack* a2) {
-	static auto fixInvStuff = Utils::FindSignature("48 89 5C 24 18 48 89 6C 24 20 56 57 41 56 48 81 EC ?? 00 00 00 48 8B ?? ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? 00 00 00 48 8B F2 48 8B F9 48 89 4C");
+	static auto fixInvStuff = Utils::FindSignature("48 89 5C ? ? 55 56 57 41 56 41 57 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 ? ? 48 8B FA 48 8B F1 48 89 4C ? ? ? ? 5A 22");
 	if (fixInvStuff)
 		reinterpret_cast<void(__fastcall*)(ItemDescriptor*, ItemStack*)>(fixInvStuff)(a1, a2);
 }
